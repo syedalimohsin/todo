9378c6f9cc86e48b0ab07866a79401b81a37eeac
syedalimohsin <syedalimohsin18@gmail.com>
2021-07-31 21:34:20 +0500

    created model and controllers

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    //
+    public function index(){
+        return 'hii';
+    }
+
+    public function create(){
+        return 'hii';
+    }
+
+    public function edit(){
+        return 'hii';
+    }
+}
